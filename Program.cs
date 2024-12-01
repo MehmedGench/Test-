03db0c77de975510b485415591bf5404ef7b9e30
MehmedGench <>
2024-12-01 09:45:49 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // this projenct is for train vagoon and persons in vagoon  , than calculate persons in vagoon  !
+
+            int countOfVagoon = int.Parse(Console.ReadLine());
+
+            int[] peopleInVagoon = new int[countOfVagoon];
+            int sum = 0;
+
+            for (int i = 0; i < countOfVagoon; i++)
+               // for (int i = 0; i < length; i++)
+                //{
+
+               // }
+            {
+                peopleInVagoon[i] = int.Parse(Console.ReadLine());
+                sum += peopleInVagoon[i];
+            }
+            Console.WriteLine(String.Join(" ", peopleInVagoon));
+            Console.WriteLine($"{sum}");
+
+
+
+        }
+
+    }
+}
